e11534a0bfa9edf74f1adf9d8eab6e8b85368f66
Donia Salah <doniasalah954@gmail.com>
2025-06-15 05:04:42 +0300

    fix: complete all test cases for calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -15,7 +15,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(3, 5), -2)
 
-    def test_multiply(self):
+    def test_multiplication(self):  # <-- دي الاسم المطلوب
         self.assertEqual(self.calc.multiply(4, 5), 20)
         self.assertEqual(self.calc.multiply(0, 5), 0)
 
